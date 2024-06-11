c4eeade73a9d585cd6fd0c00af1491f422efe443
Marie-Neige Chapel <marieneigecha@gmail.com>
2024-06-11 09:53:26 +0200

    Remove unused models

--- DeepFish/models/__init__.py
+++ DeepFish/models/__init__.py
@@ -1,5 +1,5 @@
 # DeepFish
-from . import resnet, fcn8, inception, unet, fcn8_vgg16
+from . import resnet, fcn8
 
 # -----------------------------------------------------------------------------
 def get_model(model_name, exp_dict):
@@ -10,17 +10,8 @@ def get_model(model_name, exp_dict):
 
     if model_name == "fcn8":
         model = fcn8.FCN8(n_classes)
-    
-    if model_name == "fcn8_vgg16":
-        model = fcn8_vgg16.FCN8VGG16(n_classes=n_classes)
-
-    if model_name == "unet":
-        model = unet.UNET()
 
     if model_name == "resnet":
         model = resnet.ResNet(n_classes=1)
-
-    if model_name == "inception":
-        model = inception.inceptionresnetv2(num_classes=1)
 
     return model
