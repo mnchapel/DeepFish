e9ffdc59a9526eedef2861813ef322a84223c24e
Marie-Neige Chapel <marieneigecha@gmail.com>
2024-06-11 10:35:35 +0200

    Use ResNet50FCN8 new name.

--- DeepFish/models/__init__.py
+++ DeepFish/models/__init__.py
@@ -1,5 +1,5 @@
 # DeepFish
-from . import resnet, fcn8
+from . import resnet, resnet50_fcn8
 
 # -----------------------------------------------------------------------------
 def get_model(model_name, exp_dict):
@@ -8,8 +8,8 @@ def get_model(model_name, exp_dict):
     else:
         n_classes = 2
 
-    if model_name == "fcn8":
-        model = fcn8.FCN8(n_classes)
+    if model_name == "resnet50_fcn8":
+        model = resnet50_fcn8.ResNet50FCN8(n_classes)
 
     if model_name == "resnet":
         model = resnet.ResNet(n_classes=1)
